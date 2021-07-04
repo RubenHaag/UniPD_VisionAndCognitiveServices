a1a8f472dc8395fc1caa4e3e957b5456ad3907fc
ruben.haag <ruben.haag@stud.uni-goettingen.de>
2021-07-04 12:47:17 +0200

    First working version of the AI

---
+++ MountainDataset.py
@@ -0,0 +1,51 @@
+from __future__ import print_function, division
+import os
+import torch
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import os
+
+class MountainDataset(Dataset):
+    
+    '''Mountain Peaks Dataset'''
+    def __init__(self, rootDir, bigRam = False):
+        '''
+        Args:
+            root: root directory
+            Naming should be same for DEM and Peak files
+        '''
+        
+        self.rootDir = rootDir
+        
+        self.bigRam = bigRam
+        # TODO maybe load the Peak files into bool arrays. Shouldn't be to big
+        if self.bigRam == True:
+            #If the Mashine has big Ram images can be loaded into Ram
+            #TODO load imgs
+            pass
+    
+    def __str__(self):
+        ret = "Mountain Data Set\n"
+        ret += "Root directory: {self.rootDir}"
+        ret += f"Length {len(self)}"
+        return ret
+        
+    def __len__(self):
+        return len([file for file in os.listdir(self.rootDir) if file.endswith("_dem.tif")])
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.toList()
+        
+        dem_name = os.path.join(self.rootDir, f"{idx}_dem.tif")
+        peaks_name = os.path.join(self.rootDir, f"{idx}_peaks.tif")
+        image_dem = np.expand_dims(io.imread(dem_name), 0)
+        image_peaks = np.expand_dims(io.imread(peaks_name), 0)
+        
+        sample = (image_dem, image_peaks)
+        
+        return sample
+    
--- unet_model.py
+++ unet_model.py
@@ -31,39 +31,40 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         
         self.l1_feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
             nn.ReLU()
         )
         
         self.l2_feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1),
             nn.ReLU()
         )
         
         self.l3_feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
             nn.ReLU()
         )
         
         self.l2_mapgenerator = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1),
             nn.ReLU()
         )
         self.l1_mapgenerator = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding ='same'),
+            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding ='same'),
-            nn.ReLU()
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU(),
 #TODO: Is that correct? Why two output channels? We only have one feature or not? Maybe Mountain and not Mountain?
-            nn.Conv2D(in_channels=64, out_channels=n_classes, padding="same", activation="sigmoid")
+            nn.Conv2d(kernel_size = 1, in_channels=64, out_channels=n_classes),
+            nn.ReLU()
         )
     
     def forward(self, x):
@@ -75,26 +76,31 @@ class UNet(nn.Module):
         
         l3 = nn.MaxPool2d(kernel_size=2)(l2)
         l3 = self.l3_feature_extractor(l3)
-        l3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)(l3)
         
+        l3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)(l3)
         #Cropping of second level
         dX = l2.size()[2] - l3.size()[2]
         dY = l2.size()[3] - l3.size()[3]
-        l2 = l2[:, dX//2, l3.size()[2]+dX//2, dY//2, l3.size()[3]+dY//2]
+        print(l2.size(), l3.size())
+        l2 = l2[:,:, dX//2: l3.size()[2]+dX//2, dY//2: l3.size()[3]+dY//2]
 
-        l2 = torch.cat([l2, l3], dim=0)
+        l2 = torch.cat([l2, l3], dim=1)
         
         #Generate l2 Map
         l2 = self.l2_mapgenerator(l2)
-        l2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)(l2)
+        l2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)(l2)
         # Do the same for level 3
         
         #Cropping of third level
         dX = l1.size()[2] - l2.size()[2]
         dY = l1.size()[3] - l2.size()[3]
-        l1 = l1[:, dX//2, l2.size()[2]+dX//2, dY//2, l2.size()[3]+dY//2]
-        l2 = torch.cat([l1, l2], dim=0)
+        l1 = l1[:,:, dX//2: l2.size()[2]+dX//2, dY//2: l2.size()[3]+dY//2]
+        l1 = torch.cat([l1, l2], dim=1)
         
         l1 = self.l1_mapgenerator(l1)
         
         return l1
+    
+    def save_model(self, path):
+        torch.save(self, path)
+        
